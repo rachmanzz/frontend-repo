0dfd40ebe85e61d0adee02bcfb8116ff537229fd
Muhammad Abdurrahman <rachman.sd@gmail.com>
2024-12-24 02:46:46 +0700

    build: update eslint for firebase

--- .firebase.eslintrc.js
+++ .firebase.eslintrc.js
@@ -29,5 +29,7 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "require-jsdoc": "off"
   },
 };
