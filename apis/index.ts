b81ede25ee6c7f8bd7dae23a0a94c4f1d25e152f
Muhammad Abdurrahman <rachman.sd@gmail.com>
2024-12-24 02:37:16 +0700

    feat: setting express for firebase function

--- apis/index.ts
+++ apis/index.ts
@@ -8,12 +8,20 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import * as logger from "firebase-functions/logger";
+import express from "express";
+import route from "./userRoutes";
+const app = express();
+
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+app.use(route);
+
+exports.apis = onRequest(app);
